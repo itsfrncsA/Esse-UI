01c3cb5407335ab57c06e69abe70e10b860bacf1
itsfrncsA <francisarillo@gmail.com>
2026-06-12 22:51:32 +0800

    fix: immediately show body on browser fallback

--- script.js
+++ script.js
@@ -2,10 +2,7 @@
 if (typeof GetParentResourceName === 'undefined') {
     window.GetParentResourceName = () => "my_nui_resource";
     // Auto-display the menu when viewed in a normal web browser
-    window.addEventListener('DOMContentLoaded', () => {
-        document.body.style.display = "block";
-        setSelection(0);
-    });
+    document.body.style.display = "block";
 }
 
 const menuItems = [
